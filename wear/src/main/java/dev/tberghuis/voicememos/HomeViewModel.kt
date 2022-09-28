784198894b94ee510ca73117eed4599d13a56417
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-09-29 09:04:44 +1000

    .

--- wear/src/main/java/dev/tberghuis/voicememos/HomeViewModel.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/HomeViewModel.kt
@@ -1,11 +1,7 @@
 package dev.tberghuis.voicememos
 
-import android.Manifest
 import android.content.Context
-import android.content.pm.PackageManager
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,27 +13,15 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
   private val savedStateHandle: SavedStateHandle,
   val audioRecordService: AudioRecordService,
-  // suppresslint does not remove warning
+  // suppressLint does not remove warning
   @ApplicationContext val appContext: Context,
 ) : ViewModel() {
-  val recordPermissionGranted = mutableStateOf<Boolean?>(null)
   val recordingFiles = mutableStateOf(listOf<String>())
   var recordingFilesInitialised = mutableStateOf(false)
-
-  init {
-    checkPermission()
-  }
-
-  private fun checkPermission() {
-    recordPermissionGranted.value = ContextCompat.checkSelfPermission(
-      appContext, Manifest.permission.RECORD_AUDIO
-    ) == PackageManager.PERMISSION_GRANTED
-  }
 
   fun getRecordings() {
     val files = appContext.fileList().toList().filter { it.startsWith("voicememo_") }
     recordingFiles.value = files
     recordingFilesInitialised.value = true
   }
-
 }
