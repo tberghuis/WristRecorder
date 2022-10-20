19d2f3a78396efdc352b059b5a18d9bdb6b7de2c
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-10-21 08:29:47 +1100

    .

--- wear/src/main/java/dev/tberghuis/voicememos/HomeViewModel.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/HomeViewModel.kt
@@ -20,7 +20,7 @@ class HomeViewModel @Inject constructor(
   var recordingFilesInitialised = mutableStateOf(false)
 
   fun getRecordings() {
-    val files = appContext.fileList().toList().filter { it.startsWith("wristrecorder_") }
+    val files = appContext.fileList().toList().filter { it.startsWith("wristrecorder_") }.sorted()
     recordingFiles.value = files
     recordingFilesInitialised.value = true
   }
