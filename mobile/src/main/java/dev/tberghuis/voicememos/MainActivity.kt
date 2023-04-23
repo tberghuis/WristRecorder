4e525de1fa30a67dce40206c4c5ac356b3b4f5d3
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-04-24 08:26:30 +1000

    .

--- mobile/src/main/java/dev/tberghuis/voicememos/MainActivity.kt
+++ mobile/src/main/java/dev/tberghuis/voicememos/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dev.tberghuis.voicememos.ui.theme.FreshMobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +16,9 @@ class MainActivity : ComponentActivity() {
       FreshMobileTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          MobileScreen()
         }
       }
     }
-  }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  FreshMobileTheme {
-    Greeting("Android")
   }
 }
