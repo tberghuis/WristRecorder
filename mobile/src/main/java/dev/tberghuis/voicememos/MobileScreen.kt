3d8f67ce1b7ddfbe0f848f8ea84e5a3af308674f
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-04-24 15:10:14 +1000

    .

--- mobile/src/main/java/dev/tberghuis/voicememos/MobileScreen.kt
+++ mobile/src/main/java/dev/tberghuis/voicememos/MobileScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -20,22 +21,42 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MobileScreen(
   vm: MobileViewModel = viewModel()
 ) {
   val context = LocalContext.current
 
-  Column(
-    modifier = Modifier.fillMaxSize(),
-    verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally,
-  ) {
-    Text("hello mobile screen ${vm.willitblend}")
-    Button(onClick = {
-      vm.syncRecordings(context as Activity)
-    }) {
-      Text("send message")
+
+  Scaffold(
+    bottomBar = {
+      BottomAppBar() {
+        Row(
+          modifier = Modifier.fillMaxWidth(),
+          horizontalArrangement = Arrangement.Center
+        ) {
+          Button(
+            modifier = Modifier,
+            onClick = { vm.syncRecordings(context as Activity) }) {
+            Text("Download recordings and delete from watch")
+          }
+        }
+      }
+    }
+  ) { paddingValues ->
+
+    LazyColumn(
+      modifier = Modifier
+        .padding(paddingValues)
+        .fillMaxSize(),
+    ) {
+      item {
+        Text("willitblend")
+      }
+//      items(txtFiles.value.size) { i ->
+//        Text("filename: ${txtFiles.value[i]}")
+//      }
     }
   }
 }
