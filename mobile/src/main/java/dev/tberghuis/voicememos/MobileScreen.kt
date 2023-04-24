4899eef62008844380886f505f8e3f29d3c1339d
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-04-24 16:12:58 +1000

    .

--- mobile/src/main/java/dev/tberghuis/voicememos/MobileScreen.kt
+++ mobile/src/main/java/dev/tberghuis/voicememos/MobileScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -45,17 +46,21 @@ fun MobileScreen(
 }
 
 @Composable
-fun ScreenContent(padding: PaddingValues) {
+fun ScreenContent(
+  padding: PaddingValues,
+  vm: MobileViewModel = viewModel()
+) {
+
+  val files = vm.recordingFilesStateFlow.collectAsState()
+
   LazyColumn(
     modifier = Modifier
       .padding(padding)
       .fillMaxSize(),
   ) {
-    item {
-      Text("willitblend")
+
+    items(files.value.size) { i ->
+      Text("filename: ${files.value[i].name}")
     }
-//      items(txtFiles.value.size) { i ->
-//        Text("filename: ${txtFiles.value[i]}")
-//      }
   }
 }
--- mobile/src/main/java/dev/tberghuis/voicememos/MobileViewModel.kt
+++ mobile/src/main/java/dev/tberghuis/voicememos/MobileViewModel.kt
@@ -6,14 +6,40 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.wearable.CapabilityClient
 import com.google.android.gms.wearable.Wearable
+import java.io.File
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 class MobileViewModel(private val application: Application) : AndroidViewModel(application) {
-  val willitblend = "willitblend"
+  private val dataStoreRepository = DataStoreRepository(application.applicationContext.dataStore)
 
+  val recordingFilesStateFlow = MutableStateFlow(listOf<File>())
+
+  init {
+    logd("MobileViewModel init")
+
+    viewModelScope.launch {
+      dataStoreRepository.syncRecordingsCompleteFlow.collect {
+        logd("syncRecordingsCompleteFlow $it")
+        refreshRecordingFiles()
+      }
+    }
+  }
+
+  private fun refreshRecordingFiles() {
+    val path = application.filesDir
+    val files = path.listFiles()
+    val recordingFiles = mutableListOf<File>()
+    files.forEach {
+      if (it.isFile && it.name.startsWith("wristrecorder_")) {
+        recordingFiles.add(it)
+      }
+    }
+    recordingFilesStateFlow.value = recordingFiles
+  }
 
   fun syncRecordings(activity: Activity) {
     val capabilityClient = Wearable.getCapabilityClient(activity)
