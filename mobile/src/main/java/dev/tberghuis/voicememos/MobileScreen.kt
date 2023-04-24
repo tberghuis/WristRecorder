0a0b12b133f6073e077d9b26d12c1af569302641
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-04-24 15:33:03 +1000

    .

--- mobile/src/main/java/dev/tberghuis/voicememos/MobileScreen.kt
+++ mobile/src/main/java/dev/tberghuis/voicememos/MobileScreen.kt
@@ -2,7 +2,6 @@ package dev.tberghuis.voicememos
 
 import android.app.Activity
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,15 +10,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -61,34 +57,5 @@ fun ScreenContent(padding: PaddingValues) {
 //      items(txtFiles.value.size) { i ->
 //        Text("filename: ${txtFiles.value[i]}")
 //      }
-  }
-}
-
-
-@Preview
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ScaffoldDemo() {
-  Scaffold(
-    bottomBar = {
-      BottomAppBar() {
-        Row(
-          modifier = Modifier.fillMaxWidth(),
-          horizontalArrangement = Arrangement.Center
-        ) {
-          Button(
-            modifier = Modifier,
-            onClick = {}) {
-            Text("Download recordings and delete from watch")
-          }
-        }
-      }
-    }
-  ) { paddingValues ->
-    Column(
-      modifier = Modifier.padding(paddingValues),
-    ) {
-      Text("hello preview")
-    }
   }
 }
