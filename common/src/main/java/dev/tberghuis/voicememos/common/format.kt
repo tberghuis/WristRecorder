7c6e32f1fc772d621d54c35ec495b9665e796480
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-04-25 09:25:29 +1000

    .

--- common/src/main/java/dev/tberghuis/voicememos/common/format.kt
+++ common/src/main/java/dev/tberghuis/voicememos/common/format.kt
@@ -1,9 +1,12 @@
 package dev.tberghuis.voicememos.common
 
+import android.content.Context
+import java.io.File
 import java.time.Instant
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.TimeZone
+import kotlin.math.roundToInt
 
 fun formatTimestampFromFilename(file: String): String {
   // should learn regex
@@ -14,4 +17,12 @@ fun formatTimestampFromFilename(file: String): String {
 //  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
   val formatter = DateTimeFormatter.ofPattern("EEE, d MMM HH:mm")
   return date.format(formatter)
+}
+
+
+fun calcDuration(context: Context, file: String): Int {
+  val f = File(context.filesDir, file)
+  // https://stackoverflow.com/questions/4715085/return-the-length-in-seconds-of-a-track-to-be-played-using-audiotrack
+  val duration = f.length().toFloat() / AudioController.RECORDING_RATE / 2
+  return duration.roundToInt()
 }
