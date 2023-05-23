89518edf804807fddca9353dc87b8e59e372c784
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-05-24 04:10:47 +1000

    .

---
+++ mobile/src/main/java/dev/tberghuis/voicememos/tmp/MessageActivityTmp.kt
@@ -0,0 +1,32 @@
+package dev.tberghuis.voicememos.tmp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dev.tberghuis.voicememos.ui.theme.FreshMobileTheme
+
+
+class MessageActivityTmp : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      FreshMobileTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+          TmpScreen()
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun TmpScreen() {
+  Text("hello tmp screen")
+}
