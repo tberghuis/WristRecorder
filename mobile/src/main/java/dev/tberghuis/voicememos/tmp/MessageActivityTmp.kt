c542d5a46b91925671016b16a6b57f3829fe80d1
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-05-24 05:41:33 +1000

    .

--- mobile/src/main/java/dev/tberghuis/voicememos/tmp/MessageActivityTmp.kt
+++ mobile/src/main/java/dev/tberghuis/voicememos/tmp/MessageActivityTmp.kt
@@ -1,12 +1,19 @@
 package dev.tberghuis.voicememos.tmp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,12 +36,34 @@ class MessageActivityTmp : ComponentActivity() {
   }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TmpScreen(
   vm: MessageViewModelTmp = viewModel()
+) {
+
+  Scaffold(
+    snackbarHost = {
+      SnackbarHost(vm.snackbarHostState) { data ->
+        Snackbar() {
+          Text(data.visuals.message)
+        }
+      }
+    }
+  ) {
+    TmpScreenContent(it)
+  }
+}
+
+
+@Composable
+fun TmpScreenContent(
+  paddingValues: PaddingValues,
+  vm: MessageViewModelTmp = viewModel()
 
 ) {
-  Column {
+  Column(Modifier.padding(paddingValues)) {
     Text("hello tmp screen")
     Button(onClick = {
       vm.willitblend()
@@ -42,4 +71,5 @@ fun TmpScreen(
       Text("willitblend")
     }
   }
+
 }
--- mobile/src/main/java/dev/tberghuis/voicememos/tmp/MessageViewModelTmp.kt
+++ mobile/src/main/java/dev/tberghuis/voicememos/tmp/MessageViewModelTmp.kt
@@ -1,6 +1,7 @@
 package dev.tberghuis.voicememos.tmp
 
 import android.app.Application
+import androidx.compose.material3.SnackbarHostState
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.wearable.MessageClient
@@ -16,7 +17,8 @@ class MessageViewModelTmp(private val application: Application) : AndroidViewMod
   val nodeClient = Wearable.getNodeClient(application)
 
 
-//  todo showSnackbarSharedFlow: String
+  //  todo showSnackbarSharedFlow: String
+  val snackbarHostState = SnackbarHostState()
 
 
   val messageListener = MessageClient.OnMessageReceivedListener { messageEvent ->
@@ -24,7 +26,10 @@ class MessageViewModelTmp(private val application: Application) : AndroidViewMod
 
     when (messageEvent.path) {
       "/willitblend-activity" -> {
-        logd("todo emit to snackbar shared flow")
+        logd("todo snackbar message")
+        viewModelScope.launch {
+          snackbarHostState.showSnackbar("todo snackbar message")
+        }
       }
     }
 
