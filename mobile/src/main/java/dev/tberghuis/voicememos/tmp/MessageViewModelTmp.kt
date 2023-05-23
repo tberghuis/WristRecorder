ba4336a0872fcee744c26ae1c107b4baad26cc31
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-05-24 04:28:48 +1000

    .

--- mobile/src/main/java/dev/tberghuis/voicememos/tmp/MessageViewModelTmp.kt
+++ mobile/src/main/java/dev/tberghuis/voicememos/tmp/MessageViewModelTmp.kt
@@ -2,11 +2,30 @@ package dev.tberghuis.voicememos.tmp
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.wearable.Wearable
 import dev.tberghuis.voicememos.common.logd
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 
 class MessageViewModelTmp(private val application: Application) : AndroidViewModel(application) {
+
+  val messageClient = Wearable.getMessageClient(application)
+  val nodeClient = Wearable.getNodeClient(application)
+
+
   fun willitblend() {
     logd("willitblend")
+
+
+    viewModelScope.launch {
+      val nodeId = nodeClient.localNode.await().id
+
+      val result = messageClient.sendMessage(nodeId, "/willitblend-service", byteArrayOf()).await()
+
+      logd("willitblend nodeId $nodeId result $result")
+    }
+
   }
 }
