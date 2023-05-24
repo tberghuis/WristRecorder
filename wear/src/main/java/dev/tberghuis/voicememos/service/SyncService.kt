150b0bbe63ae1aa5235c35bab59270fe3482cd11
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-05-25 00:20:51 +1000

    .

--- wear/src/main/java/dev/tberghuis/voicememos/service/SyncService.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/service/SyncService.kt
@@ -1,15 +1,12 @@
 package dev.tberghuis.voicememos.service
 
-import android.net.Uri
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.Wearable
 import com.google.android.gms.wearable.WearableListenerService
 import dev.tberghuis.voicememos.common.logd
 import java.io.BufferedInputStream
 import java.io.BufferedOutputStream
-import java.io.File
 import java.io.FileInputStream
-import java.io.FileOutputStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipOutputStream
 import kotlinx.coroutines.CoroutineScope
@@ -19,7 +16,6 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 class SyncService : WearableListenerService() {
-
   private val scope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
   private val channelClient by lazy { Wearable.getChannelClient(applicationContext) }
   private val messageClient by lazy { Wearable.getMessageClient(application) }
@@ -27,17 +23,12 @@ class SyncService : WearableListenerService() {
   override fun onMessageReceived(messageEvent: MessageEvent) {
     super.onMessageReceived(messageEvent)
     logd("onMessageReceived messageEvent $messageEvent")
-
-
     val phoneNodeId = messageEvent.data.toString(Charsets.UTF_8)
     when (messageEvent.path) {
-
       "/upload-recordings" -> {
-//        val phoneNodeId = messageEvent.data.toString(Charsets.UTF_8)
         logd("/upload-recordings phoneNodeId $phoneNodeId")
         uploadRecordingsZip(phoneNodeId)
       }
-
       "/delete-all-watch" -> {
         logd("/delete-all-watch")
         deleteAllRecordings(phoneNodeId)
@@ -49,26 +40,20 @@ class SyncService : WearableListenerService() {
     val recordingsFileList = application.filesDir.listFiles()?.filter {
       it.isFile && it.name.startsWith("wristrecorder_")
     }
-
     if (recordingsFileList.isNullOrEmpty()) {
       // send message doitwrong
       // /snackbar or /snackbar-error
-
       scope.launch {
         val ba = "No recordings".toByteArray(Charsets.UTF_8)
         messageClient.sendMessage(phoneNodeId, "/snackbar", ba).await()
       }
       return
     }
-
     logd("uploadRecordingsZip recordingsFileList $recordingsFileList")
-
     val channelTask = channelClient.openChannel(phoneNodeId, "/sendzip")
-
     scope.launch {
       val channel = channelTask.await()
       val outputStream = channelClient.getOutputStream(channel).await()
-
       ZipOutputStream(BufferedOutputStream(outputStream)).use { zos ->
         recordingsFileList.forEach { recordingFile ->
           FileInputStream(recordingFile).use { fis ->
@@ -92,12 +77,9 @@ class SyncService : WearableListenerService() {
         it.delete()
       }
     }
-
     scope.launch {
       val ba = "Delete complete".toByteArray(Charsets.UTF_8)
       messageClient.sendMessage(phoneNodeId, "/snackbar", ba).await()
     }
-
-
   }
 }
