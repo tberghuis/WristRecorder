7714aa4ed5f8cf38f5c7364de84f4b441bb4fad7
Thomas Berghuis <thomas.berghuis@gmail.com>
2023-12-31 14:44:31 +1100

    .

--- wear/src/main/java/dev/tberghuis/voicememos/tmp/TmpService.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/tmp/TmpService.kt
@@ -1,7 +1,31 @@
 package dev.tberghuis.voicememos.tmp
 
+import android.content.Intent
+import android.os.Binder
 import androidx.lifecycle.LifecycleService
+import dev.tberghuis.voicememos.common.logd
 
 class TmpService : LifecycleService() {
+
+  private val localBinder = LocalBinder()
+
+  inner class LocalBinder : Binder() {
+    internal val tmpService: TmpService
+      get() = this@TmpService
+  }
+
+  override fun onCreate() {
+    super.onCreate()
+    logd("TmpService onCreate")
+  }
+
+  override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    super.onStartCommand(intent, flags, startId)
+
+    logd("TmpService onStartCommand")
+
+    return START_NOT_STICKY
+  }
+
 
 }
