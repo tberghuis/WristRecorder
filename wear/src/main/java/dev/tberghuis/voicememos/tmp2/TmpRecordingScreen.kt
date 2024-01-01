91e2af8deb4b9de25a66af1df37b4c41c139dbf2
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-02 08:48:13 +1100

    .

--- wear/src/main/java/dev/tberghuis/voicememos/tmp2/TmpRecordingScreen.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/tmp2/TmpRecordingScreen.kt
@@ -1,19 +1,48 @@
 package dev.tberghuis.voicememos.tmp2
 
+import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Text
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun TmpRecordingScreen(
   vm: TmpRecordingVm = viewModel()
 ) {
+
+  // 33 = android 13
+  val permissionsList = if (Build.VERSION.SDK_INT >= 33) {
+    listOf(
+      android.Manifest.permission.RECORD_AUDIO,
+      android.Manifest.permission.POST_NOTIFICATIONS,
+    )
+  } else {
+    listOf(
+      android.Manifest.permission.RECORD_AUDIO,
+    )
+  }
+  val multiplePermissionsState = rememberMultiplePermissionsState(
+    permissionsList
+  )
+
+  if (!multiplePermissionsState.allPermissionsGranted) {
+    LaunchedEffect(Unit) {
+      multiplePermissionsState.launchMultiplePermissionRequest()
+    }
+  }
+
+
+
   Column(
     modifier = Modifier.fillMaxSize(),
     verticalArrangement = Arrangement.Center,
