9ef071e03c4996214e16b295669c75d64a0aa855
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-02 09:30:55 +1100

    .

--- wear/src/main/java/dev/tberghuis/voicememos/tmp2/TmpRecordingScreen.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/tmp2/TmpRecordingScreen.kt
@@ -1,5 +1,8 @@
 package dev.tberghuis.voicememos.tmp2
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,12 +17,16 @@ import androidx.wear.compose.material.Text
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionStatus
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import android.provider.Settings
+import androidx.compose.ui.platform.LocalContext
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun TmpRecordingScreen(
   vm: TmpRecordingVm = viewModel()
 ) {
+
+  val context = LocalContext.current
 
   // 33 = android 13
   val permissionsList = if (Build.VERSION.SDK_INT >= 33) {
@@ -52,28 +59,35 @@ fun TmpRecordingScreen(
     horizontalAlignment = Alignment.CenterHorizontally,
   ) {
     Text("tmp recording screen")
-
     if (recordPermissionState.status == PermissionStatus.Denied(false)) {
       // doitwrong
-      // this is also displayed if user dismisses permission prompt
-      Text("please enable recording permission in system settings")
-
-      // todo button to system settings
-
-      return
-    }
-
-
-    Button(onClick = {
-      if (recordPermissionState.status is PermissionStatus.Denied) {
-        recordPermissionState.launchPermissionRequest()
-      } else {
-        vm.startRecording()
+      // this is also displayed if user dismisses permission prompt on app start
+      Text("please enable Microphone permission in system settings")
+      Button(onClick = {
+        launchPermissionsSettings(context)
+      }) {
+        Text("system settings")
       }
-
-
-    }) {
-      Text("start")
+    } else { // microphone permission granted || shouldShowRationale == true
+      Button(onClick = {
+        if (recordPermissionState.status is PermissionStatus.Denied) {
+          recordPermissionState.launchPermissionRequest()
+        } else {
+          vm.startRecording()
+        }
+      }) {
+        Text("start")
+      }
     }
   }
 }
+
+
+private fun launchPermissionsSettings(context: Context) {
+  val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+  val uri = Uri.fromParts("package", context.packageName, null)
+  intent.setData(uri)
+  context.startActivity(intent)
+}
+
+
