4c8846d2bd9f23dcc970635c6ff0cfa8231377b0
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-02 10:22:08 +1100

    .

--- wear/src/main/java/dev/tberghuis/voicememos/tmp/TmpScreen.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/tmp/TmpScreen.kt
@@ -1,24 +1,15 @@
 package dev.tberghuis.voicememos.tmp
 
-import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Text
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.rememberPermissionState
-import dev.tberghuis.voicememos.common.logd
 
 @Composable
 fun TmpScreen(
@@ -50,31 +41,9 @@ fun TmpScreen(
     }
     Row {
 
-      PermissionButton()
 
     }
 
 
-  }
-}
-
-
-@OptIn(ExperimentalPermissionsApi::class)
-@Composable
-fun PermissionButton(
-  vm: TmpVm = viewModel()
-) {
-  // 33 = android 13
-  val permissionState = if (Build.VERSION.SDK_INT >= 33) {
-    rememberPermissionState(
-      android.Manifest.permission.POST_NOTIFICATIONS
-    )
-  } else {
-    TODO("VERSION.SDK_INT < TIRAMISU")
-  }
-  Button(onClick = {
-    permissionState.launchPermissionRequest()
-  }) {
-    Text("permission")
   }
 }
