2b1d4c19343420591408ea75e60a3689ced78516
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-02 13:53:03 +1100

    .

--- wear/src/main/java/dev/tberghuis/voicememos/page/RecordingPage.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/page/RecordingPage.kt
@@ -1,57 +1,96 @@
 package dev.tberghuis.voicememos.page
 
+import android.os.Build
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Text
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionStatus
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
-import dev.tberghuis.voicememos.common.logd
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import dev.tberghuis.voicememos.composables.RecordingUi
+import dev.tberghuis.voicememos.tmp2.launchPermissionsSettings
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun XxxRecordingPage(
+fun RecordingPage(
   navigateRecordingDetail: (String) -> Unit
 ) {
-  val recordPermissionState = rememberPermissionState(
-    android.Manifest.permission.RECORD_AUDIO
+  val context = LocalContext.current
+  val permissionsList = if (Build.VERSION.SDK_INT >= 33) {
+    listOf(
+      android.Manifest.permission.RECORD_AUDIO,
+      android.Manifest.permission.POST_NOTIFICATIONS,
+    )
+  } else {
+    listOf(
+      android.Manifest.permission.RECORD_AUDIO,
+    )
+  }
+  val multiplePermissionsState = rememberMultiplePermissionsState(
+    permissionsList
   )
-  Column(
-    Modifier.fillMaxSize(),
-    horizontalAlignment = Alignment.CenterHorizontally,
-    verticalArrangement = Arrangement.Center,
-  ) {
-    when (recordPermissionState.status) {
-      PermissionStatus.Granted -> {
-        RecordingUi(navigateRecordingDetail)
-      }
-      is PermissionStatus.Denied -> {
-        LaunchedEffect(Unit) {
-          logd("effect")
-          recordPermissionState.launchPermissionRequest()
-        }
-        val shouldShowRationale = recordPermissionState.status.shouldShowRationale
-        logd("shouldShowRationale $shouldShowRationale")
 
-        if (shouldShowRationale) {
-          Button(onClick = {
-            logd("enable")
-            recordPermissionState.launchPermissionRequest()
-          }) {
-            Text("Allow Record Permission")
+  if (!multiplePermissionsState.allPermissionsGranted) {
+    LaunchedEffect(Unit) {
+      multiplePermissionsState.launchMultiplePermissionRequest()
+    }
+  }
+
+  val recordPermissionState = multiplePermissionsState.permissions.find {
+    it.permission == android.Manifest.permission.RECORD_AUDIO
+  }!!
+
+  Box(
+    modifier = Modifier.fillMaxSize(),
+    contentAlignment = Alignment.Center,
+  ) {
+    Column(
+      Modifier
+//      .fillMaxSize(0.8f),
+        .fillMaxWidth(0.8f)
+        .fillMaxHeight(),
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.Center,
+    ) {
+      when (recordPermissionState.status) {
+        // user denied 0 or 2 times (prompt dialog dismissed when 0)
+        PermissionStatus.Denied(false) -> {
+          Text(
+            "Please enable Microphone permission in settings",
+            textAlign = TextAlign.Center,
+          )
+          Button(
+            onClick = {
+              launchPermissionsSettings(context)
+            },
+            modifier = Modifier.fillMaxWidth(),
+          ) {
+            Text("Show Settings")
           }
-        } else {
-          Text("Please enable Microphone permission in settings", textAlign = TextAlign.Center)
         }
+
+        PermissionStatus.Denied(true) -> {
+          RecordingUi(navigateRecordingDetail) {
+            recordPermissionState.launchPermissionRequest()
+          }
+        }
+
+        PermissionStatus.Granted -> {
+          RecordingUi(navigateRecordingDetail)
+        }
+        // redundant
+        else -> {}
       }
     }
   }
