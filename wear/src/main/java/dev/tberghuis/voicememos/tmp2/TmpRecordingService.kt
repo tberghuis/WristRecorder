ff2b9b11cc8a13a1e3060508a1610ce5db10d585
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-03 11:35:15 +1100

    .

--- wear/src/main/java/dev/tberghuis/voicememos/tmp2/TmpRecordingService.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/tmp2/TmpRecordingService.kt
@@ -1,13 +1,16 @@
 package dev.tberghuis.voicememos.tmp2
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.lifecycleScope
 import dev.tberghuis.voicememos.common.AudioController
 import dev.tberghuis.voicememos.common.logd
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class TmpRecordingService : LifecycleService() {
   val isRecordingFlow = MutableStateFlow(false)
@@ -39,16 +42,20 @@ class TmpRecordingService : LifecycleService() {
   }
 
   // doitwrong
+  @SuppressLint("MissingPermission")
   fun startRecording() {
     logd("startRecording")
     isRecordingFlow.value = true
-    // todo set filename in callback
+    // do i need to change dispatcher?
+    recordingJob = lifecycleScope.launch {
+      audioController.record { filename = it }
+    }
   }
 
   fun stopRecording(): String? {
     logd("stopRecording")
     isRecordingFlow.value = false
+    recordingJob?.cancel()
     return filename
   }
-
 }
