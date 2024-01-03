2255be9eb7c60b375fd5eca8fa8909b76df51085
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-03 11:23:49 +1100

    .

--- wear/src/main/java/dev/tberghuis/voicememos/tmp2/TmpRecordingService.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/tmp2/TmpRecordingService.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import androidx.lifecycle.LifecycleService
+import dev.tberghuis.voicememos.common.AudioController
 import dev.tberghuis.voicememos.common.logd
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class TmpRecordingService : LifecycleService() {
-
+  private lateinit var audioController: AudioController
+  var recordingJob: Job? = null
   val isRecordingFlow = MutableStateFlow(false)
   var filename: String? = null
 
@@ -24,6 +27,11 @@ class TmpRecordingService : LifecycleService() {
     return localBinder
   }
 
+  override fun onCreate() {
+    super.onCreate()
+    audioController = AudioController(application)
+  }
+
   override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
     super.onStartCommand(intent, flags, startId)
     return START_NOT_STICKY
@@ -32,11 +40,13 @@ class TmpRecordingService : LifecycleService() {
   // doitwrong
   fun startRecording() {
     logd("startRecording")
+    isRecordingFlow.value = true
     // todo set filename in callback
   }
 
   fun stopRecording(): String? {
     logd("stopRecording")
+    isRecordingFlow.value = false
     return filename
   }
 
