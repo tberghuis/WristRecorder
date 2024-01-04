aa77f5035e3d66060119580626b0165de4c997c3
Thomas Berghuis <thomas.berghuis@gmail.com>
2024-01-04 12:52:59 +1100

    .

--- wear/src/main/java/dev/tberghuis/voicememos/page/RecordingList.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/page/RecordingList.kt
@@ -2,8 +2,9 @@ package dev.tberghuis.voicememos.page
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -12,11 +13,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.Chip
+import androidx.wear.compose.material.ChipDefaults
 import androidx.wear.compose.material.Text
 import com.google.android.horologist.annotations.ExperimentalHorologistApi
 import com.google.android.horologist.compose.layout.ScalingLazyColumn
+import com.google.android.horologist.compose.layout.ScalingLazyColumnDefaults.responsive
 import com.google.android.horologist.compose.layout.ScreenScaffold
 import com.google.android.horologist.compose.layout.rememberColumnState
 import dev.tberghuis.voicememos.HomeViewModel
@@ -43,33 +47,41 @@ fun RecordingList(
     return
   }
 
-  val columnState = rememberColumnState()
+  val columnState = rememberColumnState(responsive())
+
   ScreenScaffold(scrollState = columnState) {
     ScalingLazyColumn(
       columnState = columnState,
       modifier = Modifier.fillMaxSize(),
     ) {
+      // this is to ensure i pass play store review:
+      // Fits within the physical display area.
+      // No text or controls are cut off by the screen edges.
+      item {
+        Spacer(Modifier.height(5.dp))
+      }
       items(viewModel.recordingFiles.value.size) { i ->
         val file = viewModel.recordingFiles.value[i]
         val buttonText = remember(file, context) {
           "${formatTimestampFromFilename(file)} ${calcDuration(context, file)}s"
         }
-        Button(modifier = Modifier.fillMaxWidth(),
+
+        Chip(
+          label = {
+            Text(
+              buttonText, Modifier.padding(horizontal = 5.dp),
+              fontSize = 12.sp,
+              overflow = TextOverflow.Ellipsis,
+              maxLines = 1,
+            )
+          },
           onClick = {
             logd("play $file")
             onRecordingClick(file)
-          }
-        ) {
-          Text(
-            buttonText, Modifier.padding(horizontal = 10.dp),
-            overflow = TextOverflow.Ellipsis,
-            maxLines = 1,
-          )
-        }
+          },
+          contentPadding = ChipDefaults.CompactChipTapTargetPadding,
+        )
       }
     }
-
   }
-
-
 }
