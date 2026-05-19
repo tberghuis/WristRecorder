d59018365261d10a8bf19c3f9b009b1cc1a8d324
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-05-19 13:02:55 +1000

    .

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   alias(libs.plugins.android.library)
-  alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -22,9 +21,6 @@ android {
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
-  }
-  kotlinOptions {
-    jvmTarget = "11"
   }
   buildFeatures {
     buildConfig = true
--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
   alias(libs.plugins.android.application)
-  alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
 }
 
@@ -39,9 +38,6 @@ android {
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
-  }
-  kotlinOptions {
-    jvmTarget = "17"
   }
   buildFeatures {
     compose = true
