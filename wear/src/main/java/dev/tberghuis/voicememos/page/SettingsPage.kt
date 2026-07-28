16f32f64f266b6244e11e053024c3051b6da3cce
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-07-28 11:45:07 +1000

    .

--- wear/src/main/java/dev/tberghuis/voicememos/page/SettingsPage.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/page/SettingsPage.kt
@@ -1,8 +1,11 @@
 package dev.tberghuis.voicememos.page
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.wear.compose.material.Text
 import androidx.wear.compose.material3.SwitchButton
@@ -11,7 +14,11 @@ import androidx.wear.compose.material3.SwitchButton
 fun SettingsPage() {
   // doitwrong
   // look at google samples for settings screen
-  Column {
+  Column(
+    modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
+  ) {
     // todo settings cog icon as heading
     Text("Settings")
     SwitchButton(
