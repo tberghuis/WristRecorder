6c4affd7bebe4bfa68a71b54c5ed03962d1bf53c
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-07-31 09:38:22 +1000

    .

--- wear/src/main/java/dev/tberghuis/voicememos/page/SettingsPage.kt
+++ wear/src/main/java/dev/tberghuis/voicememos/page/SettingsPage.kt
@@ -1,15 +1,23 @@
 package dev.tberghuis.voicememos.page
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.wear.compose.material.Text
+import androidx.wear.compose.material3.Icon
 import androidx.wear.compose.material3.SwitchButton
 import dev.tberghuis.voicememos.viewmodels.SettingsVm
 
@@ -17,25 +25,31 @@ import dev.tberghuis.voicememos.viewmodels.SettingsVm
 fun SettingsPage(
   vm: SettingsVm = viewModel()
 ) {
-
   val backOverride by vm.backOverride.collectAsState()
-
-  // doitwrong
-  // look at google samples for settings screen
-  Column(
+  Box(
     modifier = Modifier.fillMaxSize(),
-    verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally,
+    contentAlignment = Alignment.Center,
   ) {
-    // todo settings cog icon as heading
-    Text("Settings")
-    SwitchButton(
-      checked = backOverride,
-      onCheckedChange = { vm.toggleBackOverride() },
-      modifier = Modifier,
-      enabled = true,
+    Column(
+      modifier = Modifier
+        .fillMaxWidth(0.8f)
+        .fillMaxHeight(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-      Text("override back button to toggle recording")
+      Icon(
+        imageVector = Icons.Default.Settings,
+        contentDescription = "Settings",
+        modifier = Modifier.padding(bottom = 8.dp),
+      )
+      SwitchButton(
+        checked = backOverride,
+        onCheckedChange = { vm.toggleBackOverride() },
+        modifier = Modifier,
+        enabled = true,
+      ) {
+        Text("override back button to toggle recording")
+      }
     }
   }
 }
