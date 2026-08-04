8cbbbab8e82a323835846be02b1c53985fba1284
Thomas Berghuis <thomas.berghuis@gmail.com>
2026-08-04 11:17:36 +1000

    .

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "dev.tberghuis.wristrecorder"
     minSdk = 28
     targetSdk = 36
-    versionCode = 10028
-    versionName = "1.13.0-mobile"
+    versionCode = 10029
+    versionName = "1.13.2-mobile"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables {
